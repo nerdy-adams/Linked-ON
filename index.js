87c69461b295dc33f527b1a8c90d1b85c88ed443
nerdrun <nerdrun.kai@gmail.com>
2018-06-23 18:48:25 +0900

    searchEngine enter event and _self

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
   const searchFilter = document.getElementById('search-filter');
   const searchInput = document.getElementById('search-input');
   const searchBtn = document.getElementById('search-btn');
-
-  searchBtn.addEventListener('click', () => {
+  const searchEngine = () => {
     let searchValue = searchInput.value;
     if(!searchValue) return;
     if(!searchFilter.selectedIndex) searchValue = searchValue.replace(/ /g, '+');
-    this.open(searchFilter.value + searchValue);
+    this.open(searchFilter.value + searchValue, '_self');
+  }
+  window.addEventListener('keyup', (event) => {
+    const code = event.which || event.keyCode;
+    if(code === 13) searchEngine();
   });
+  searchBtn.addEventListener('click', searchEngine);
 })();
